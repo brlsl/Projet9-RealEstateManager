8cdeea094e549be427448a84dd4658850410ea75
Bruno LAN SUN LUK <lansunluk@gmail.com>
2020-08-18 01:57:43 +0200

    ImageDataRepository added to Injection.java

--- app/src/main/java/com/openclassrooms/realestatemanager/injection/Injection.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/injection/Injection.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.openclassrooms.realestatemanager.database.RealEstateManagerDatabase;
 import com.openclassrooms.realestatemanager.repositories.AgentDataRepository;
+import com.openclassrooms.realestatemanager.repositories.ImageDataRepository;
 import com.openclassrooms.realestatemanager.repositories.PropertyDataRepository;
 
 import java.util.concurrent.Executor;
@@ -20,12 +21,18 @@ public class Injection {
         return new PropertyDataRepository(database.propertyDao());
     }
 
+    public static ImageDataRepository provideImageDataSource(Context context){
+        RealEstateManagerDatabase database = RealEstateManagerDatabase.getInstance(context);
+        return new ImageDataRepository(database.imageDao());
+    }
+
     public static Executor provideExecutor(){return Executors.newSingleThreadExecutor(); }
 
     public static ViewModelFactory provideViewModelFactory(Context context){
         AgentDataRepository dataSourceAgent = provideAgentDataSource(context);
         PropertyDataRepository dataSourceProperty = providePropertyDataSource(context);
+        ImageDataRepository dataSourceImage = provideImageDataSource(context);
         Executor executor = provideExecutor();
-        return new ViewModelFactory(dataSourceAgent,dataSourceProperty, imageDataSource, executor);
+        return new ViewModelFactory(dataSourceAgent,dataSourceProperty, dataSourceImage, executor);
     }
 }
