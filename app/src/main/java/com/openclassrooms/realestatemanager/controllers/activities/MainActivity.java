9e5e7d4261fa498d49fb1a7488a76097bde5c0ad
Bruno LAN SUN LUK <lansunluk@gmail.com>
2020-08-28 12:06:04 +0200

    prevent fragment recreation when view is two pane toolbar menu added

--- app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/MainActivity.java
@@ -2,8 +2,12 @@ package com.openclassrooms.realestatemanager.controllers.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
@@ -23,6 +27,7 @@ public class MainActivity extends BaseActivity implements PropertiesListFragment
     private boolean isTwoPane;
     //private REMViewModel mViewModel;
     PropertyDetailFragment mPropertyDetailFragment = new PropertyDetailFragment();
+    long mPropertyId = -1, mAgentId = -1;
 
     // FOR UI
     private androidx.appcompat.widget.Toolbar mToolbar;
@@ -38,7 +43,12 @@ public class MainActivity extends BaseActivity implements PropertiesListFragment
         configureDrawerLayout();
         configureDualPaneLayout(savedInstanceState);
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu); // make toolbar menu item appear
+        return true;
     }
 
     private void configureDrawerLayout() {
@@ -65,37 +75,40 @@ public class MainActivity extends BaseActivity implements PropertiesListFragment
         }
 
         if (isTwoPane){
-            mFragmentManager.beginTransaction()
-                    .replace(R.id.container, new PropertiesListFragment())
-                    .replace(R.id.detail_container, mPropertyDetailFragment).hide(mPropertyDetailFragment)
-                    .commit();
+                mFragmentManager.beginTransaction()
+                        .replace(R.id.container, new PropertiesListFragment())
+                        .commit();
         }
     }
 
-
     @Override
     public void onItemPropertySelected(Property property) {
-        PropertyDetailFragment fragment = new PropertyDetailFragment();
+        if (mPropertyId != property.getId() || mAgentId != property.getAgentId()) { // prevent fragment recreation
+            mPropertyId = property.getId();
+            mAgentId = property.getAgentId();
 
-        Bundle bundle = new Bundle();
+            Bundle bundle = new Bundle();
+            bundle.putLong(PROPERTY_ID_KEY, mPropertyId);
+            bundle.putLong(PROPERTY_AGENT_ID_KEY, mAgentId);
+            PropertyDetailFragment fragment = new PropertyDetailFragment();
+            fragment.setArguments(bundle);
 
-        bundle.putLong(PROPERTY_ID_KEY, property.getId());
-        bundle.putLong(PROPERTY_AGENT_ID_KEY, property.getAgentId());
-        fragment.setArguments(bundle);
-
-
-        if(isTwoPane){
-            mFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.detail_container, fragment)
-                    .commit();
-        }
-        else{
-            mFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.container, fragment)
-                    .addToBackStack(null)
-                    .commit();
+            if(isTwoPane){
+                if (fragment != mPropertyDetailFragment) {
+                    mPropertyDetailFragment = fragment;
+                    mFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.detail_container, fragment)
+                            .commit();
+                }
+            }
+            else{
+                mFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.container, fragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
         }
     }
 
@@ -113,5 +126,18 @@ public class MainActivity extends BaseActivity implements PropertiesListFragment
     private void configureToolBar() {
         this.mToolbar = findViewById(R.id.toolbar_main_activity);
         setSupportActionBar(mToolbar);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.search_property_toolbar:
+                Toast.makeText(this, "Open search property", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;
     }
 }
